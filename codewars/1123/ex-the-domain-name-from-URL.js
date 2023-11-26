b87053c9d56eaef545dc2e92e3f9a91847373c5e
maksimput <82806350+maksimput@users.noreply.github.com>
2023-11-26 17:24:55 +0500

    GUI test 2

--- codewars/1123/ex-the-domain-name-from-URL.js
+++ codewars/1123/ex-the-domain-name-from-URL.js
@@ -2,6 +2,6 @@ function domainName(url){
     url = url.replace("https://", '');
     url = url.replace("http://", '');
     url = url.replace("www.", '');
-    return url.split('.')[11];
+    return url.split('.')[0];
   };
   console.log(domainName('www.google.com'))
